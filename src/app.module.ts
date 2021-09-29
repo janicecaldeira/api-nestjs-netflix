f464334565e8140d23cef689fc14e435afc51c59
janicecaldeira <janicecaldeira@gmail.com>
2021-09-28 22:24:48 -0300

    Criando relacionamento com banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { FilmesModule } from './filmes/filmes.module';
 import { ParticipantesModule } from './participantes/participantes.module';
 import { GenerosModule } from './generos/generos.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FilmesModule, ParticipantesModule, GenerosModule],
+  imports: [FilmesModule, ParticipantesModule, GenerosModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
