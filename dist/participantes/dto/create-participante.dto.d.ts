4cbbe1059d027308a591d6a2af6b5e93412529dc
janicecaldeira <janicecaldeira@gmail.com>
2021-09-30 22:51:56 -0300

    Inserindo rota post

---
+++ dist/participantes/dto/create-participante.dto.d.ts
@@ -0,0 +1,8 @@
+export declare class CreateParticipanteDto {
+    nome: string;
+    imagem: string;
+    data_nascimento: Date;
+    isStaff: boolean;
+    isAtor: boolean;
+    filmeId: number;
+}
--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateFilmeDto } from './dto/create-filme.dto';
+import { FilmesService } from './filmes.service';
+import { Filme } from '.prisma/client';
 
-@Controller()
-export class FilmesController {}
+@Controller('filmes')
+export class FilmesController {
+  constructor(private filmesService: FilmesService) {}
+
+  @Post('/create')
+  @UsePipes(ValidationPipe)
+  async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+    return this.filmesService.createFilme(createFilme);
+  }
+
+  // @Get('/findAll')
+  // @UsePipes(ValidationPipe)
+  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+  //   return this.filmesService.createFilme(createFilme);
+  // }
+
+  // @Get('/findOne/:id')
+  // @UsePipes(ValidationPipe)
+  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+  //   return this.filmesService.createFilme(createFilme);
+  // }
+
+  // @Put('/update/:id')
+  // @UsePipes(ValidationPipe)
+  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+  //   return this.filmesService.createFilme(createFilme);
+  // }
+
+  // @Delete('/deleteOne/:id')
+  // @UsePipes(ValidationPipe)
+  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+  //   return this.filmesService.createFilme(createFilme);
+  // }
+
+  // @Delete('/deleteAll')
+  // @UsePipes(ValidationPipe)
+  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
+  //   return this.filmesService.createFilme(createFilme);
+  // }
+}
