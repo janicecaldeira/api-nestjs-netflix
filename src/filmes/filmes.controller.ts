f51be0351c1702df7393c0ad9f80da717f149a1d
janicecaldeira <janicecaldeira@gmail.com>
2021-09-30 23:02:18 -0300

    Atualizando filme controller

--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -21,31 +21,16 @@ export class FilmesController {
 
   // @Get('/findAll')
   // @UsePipes(ValidationPipe)
-  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
-  //   return this.filmesService.createFilme(createFilme);
-  // }
 
   // @Get('/findOne/:id')
   // @UsePipes(ValidationPipe)
-  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
-  //   return this.filmesService.createFilme(createFilme);
-  // }
 
   // @Put('/update/:id')
   // @UsePipes(ValidationPipe)
-  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
-  //   return this.filmesService.createFilme(createFilme);
-  // }
 
   // @Delete('/deleteOne/:id')
   // @UsePipes(ValidationPipe)
-  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
-  //   return this.filmesService.createFilme(createFilme);
-  // }
 
   // @Delete('/deleteAll')
   // @UsePipes(ValidationPipe)
-  // async create(@Body() createFilme: CreateFilmeDto): Promise<Filme> {
-  //   return this.filmesService.createFilme(createFilme);
-  // }
 }
