52e8b4e5593a008ffbdb8cd2dec844f02e53cd97
janicecaldeira <janicecaldeira@gmail.com>
2021-10-01 21:34:22 -0300

    Correção exports

--- dist/prisma/prisma.module.js
+++ dist/prisma/prisma.module.js
@@ -14,6 +14,7 @@ let PrismaModule = class PrismaModule {
 PrismaModule = __decorate([
     (0, common_1.Module)({
         providers: [prisma_service_1.PrismaService],
+        exports: [prisma_service_1.PrismaService],
     })
 ], PrismaModule);
 exports.PrismaModule = PrismaModule;
